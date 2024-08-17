4fcf4909f68efc6edfb491a82f2161f338c3c647
aykutkaraca <karaca.aykut@gmail.com>
2024-08-17 21:05:55 +0300

    changed intro text

--- main.js
+++ main.js
@@ -132,7 +132,7 @@ export {playticksound};
             surname="the";
         }
         
-        document.getElementById('intro').innerHTML= "This is a gender reveal spin the wheel for <strong>" + surname + "</strong> family. It contains sound when the gender is revealed. Do you want to continue with sound?";
+        document.getElementById('intro').innerHTML= "This is a gender reveal spin the wheel for <strong>" + surname + "</strong> family. It contains high level sound. Do you want to continue with sound?";
         document.getElementById('id01').style.display='block';
         $('.nosoundbtn').on("click", function (e) {
             document.getElementById('id01').style.display='none';
